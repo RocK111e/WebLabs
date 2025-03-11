84ffc3e56ba9273dd8f3e45e904d5d8ef24d17da
RocK111e <romanmeg9@gmail.com>
2025-03-11 02:36:26 +0200

    sidebar working

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const bur_but = document.getElementById("burger-btn");
+    bur_but.addEventListener("click", burger_menu);
+});
+
 function burger_menu() {
-    var menu = document.getElementById("side_bar");
+    console.log("burger menu clicked");
+    var menu = document.getElementById("sidebar");  // Updated to "sidebar"
     var btn = document.querySelector(".burger-btn");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+    
+    if (menu.classList.contains("open")) {
+        menu.classList.remove("open");
         btn.textContent = "☰";
     } else {
-        menu.style.display = "block";
+        menu.classList.add("open");
         btn.textContent = "✕";
     }
 }
