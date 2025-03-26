f8465028e417e56977d08467770274843986d5c3
RocK111e <romanmeg9@gmail.com>
2025-03-26 11:25:43 +0200

    Add fixed sw

--- sw.js
+++ sw.js
@@ -47,7 +47,6 @@ self.addEventListener("install", (event) => {
     event.respondWith(
       caches.match(event.request)
         .then(response => response || fetch(event.request))
-        .catch(() => caches.match('/index.html'))
     );
   });
   
